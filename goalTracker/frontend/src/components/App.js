78d8c5e8f613c3869537981f6f2e1feb9a76e12f
Isaac Wengler <iwengler3@gmail.com>
2021-03-07 22:36:25 -0600

    integrated react

---
+++ goalTracker/frontend/src/components/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+    render() {
+        return <h1>Goal Tracker</h1>
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
